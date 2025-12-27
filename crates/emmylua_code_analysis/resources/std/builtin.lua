4ad70f5b15f0ab44fefea8b4e002f62036819c56
xuhuanzy <501417909@qq.com>
2025-12-27 11:16:49 +0800

    refactor(builtin): 更新内置属性以处理luban配置表

--- crates/emmylua_code_analysis/resources/std/builtin.lua
+++ crates/emmylua_code_analysis/resources/std/builtin.lua
@@ -166,37 +166,15 @@
 ---@attribute deprecated(message: string?)
 
 ---
---- Language Server Optimization Items.
----
---- Parameters:
---- - `check_table_field`: Skip the assign check for table fields. It is recommended to use this option for all large configuration tables.
---- - `delayed_definition`: Indicates that the type of the variable is determined by the first assignment.
----    Only valid for `local` declarations with no initial value.
----@attribute lsp_optimization(code: "check_table_field"|"delayed_definition")
+--- 将一个表作为配置表处理. 会为其启用一系列配置表相关的功能.
+---@attribute config()
 
 ---
---- Index field alias, will be displayed in `hint` and `completion`.
----
---- Receives a string parameter for the alias name.
----@attribute index_alias(name: string)
+--- 标记枚举为位域, 位域的值为2的幂次.
+---@attribute flags()
 
 ---
---- This attribute must be applied to function parameters, and the function parameter's type must be a string template generic,
---- used to specify the default constructor of a class.
+--- 检查某字段是否为某配置表的合法 key.
 ---
---- Parameters:
---- - `name`: The name of the method as a constructor.
---- - `root_class`: Used to mark the root class, will implicitly inherit this class, such as `System.Object` in c#. Defaults to empty.
---- - `strip_self`: Whether the `self` parameter can be omitted when calling the constructor, defaults to `true`
---- - `return_self`: Whether the constructor is forced to return `self`, defaults to `true`
----@attribute constructor(name: string, root_class: string?, strip_self: boolean?, return_self: boolean?)
-
----
---- Associates `getter` and `setter` methods with a field. Currently provides only definition navigation functionality,
---- and the target methods must reside within the same class.
----
---- Parameters:
---- - `convention`: Naming convention, defaults to `camelCase`. Implicitly adds `get` and `set` prefixes. eg: `_age` -> `getAge`, `setAge`.
---- - `getter`: Getter method name. Takes precedence over `convention`.
---- - `setter`: Setter method name. Takes precedence over `convention`.
----@attribute field_accessor(convention: "camelCase"|"PascalCase"|"snake_case"|nil, getter: string?, setter: string?)
+--- 为其启用诊断与补全.
+---@attribute Validator.ref(t: string)
